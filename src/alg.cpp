06205e4379521a6b5f21b07b49a207199e351bee
k1fir4ig <maxim140906@gmail.com>
2026-05-26 00:41:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,41 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double res = 1.0;
+  for (uint16_t i = 1; i <= n; i++) res *= value;
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t res = 1;
+  for (uint16_t i = 1; i <= n; i++) res *= i;
+  return res;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  double res = pown(x, n) / fact(n);
+  return res;
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double res = 1.0;
+  for (int i = 1; i <= count; i++)
+    res += pown(x, i) / static_cast<double>(fact(i));
+  return res;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double res = 0.0;
+  for (int i = 1; i <= count; i++)
+    res += pown(-1.0, i - 1) * pown(x, 2 * i - 1) /
+           static_cast<double>(fact(2 * i - 1));
+  return res;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double res = 0.0;
+  for (int i = 1; i <= count; i++)
+    res += pown(-1.0, i - 1) * pown(x, 2 * i - 2) /
+           static_cast<double>(fact(2 * i - 2));
+  return res;
 }
